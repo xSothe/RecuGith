75a01f9be1dd5c67b172d22ad7a39c7c69053f5e
unknown <xSothe>
2023-05-06 22:54:39 +0200

    encapsulando toString de Empleado

--- project/untitled/src/Empleado.java
+++ project/untitled/src/Empleado.java
@@ -8,5 +8,9 @@ class Empleado {
         this.nombre = name;
         this.birthDate = birthDate;
     }
+    @Override
+    public String toString() {
+        return "Empleado: " + nombre + " (nacido en 19" + birthDate.getYear() + ")";
+    }
 
 }
